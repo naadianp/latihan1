ccfe854c5b1de4a0c17b0cbf706bc16cf3da5725
naadianp <nadia.nur7806@smk.belajar.id>
2024-11-18 11:45:09 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,14 @@
+<?php
+$a=12;
+$b=1;
+
+$penambahan = $a + $b;
+$pengurangan = $a - $b;
+$perkalian = $a * $b;
+$pembagian = $a / $b;
+
+echo $penambahan;
+//echo $pengurangan;
+//echo $perkalian;
+//echo $pembagian;
+?>
